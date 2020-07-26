234d8d204d59887a461d2a88aabae352c83d2d28
CESAR TZOC <csartalva01@gmail.com>
2020-07-26 17:05:56 -0600

    Se agregaron los componentes para el formulario que solicita las secuencias, ya funcional

--- src/main/java/decodificador/main/Main.java
+++ src/main/java/decodificador/main/Main.java
@@ -7,7 +7,7 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
         //Agregar codigo aquí
-        System.out.println("hola mundo");
+        //System.out.println("hola mundo");
         JFrame view = new MainView("Decodificador ADN");
     }
 }
--- src/main/java/decodificador/vista/MainView.java
+++ src/main/java/decodificador/vista/MainView.java
@@ -1,19 +1,47 @@
 package decodificador.vista;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MainView extends JFrame {
 
     private JPanel mainPanel;
+    private JButton decodificarButton;
+    private JTextField sequenceAText;
+    private JTextField sequenceBText;
+    private JLabel mainLabel;
+    private JLabel sequenceALabel;
+    private JLabel sequenceBLabel;
+    private JLabel resultLabel;
+    private JLabel sequenceResult;
 
     public MainView(String title) {
         super(title);
 
         this.setContentPane(mainPanel);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(400, 400);
+        this.setSize(500, 600);
         this.setResizable(false);
         this.setLocationRelativeTo(null);
         this.setVisible(true);
+        decodificarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String sequenceA = sequenceAText.getText();
+                String sequenceB = sequenceBText.getText();
+                String result;
+
+                if(!sequenceA.isEmpty() && !sequenceB.isEmpty()) {
+                    //Acciones para decodificar
+
+                    result = "RESULTADO";
+                    sequenceResult.setText(result);
+                } else {
+                    //System.out.println("Cadenas vacias");
+                    JOptionPane.showMessageDialog(null, "Debe llenar ambos campos para poder Decodificar", "ERROR", JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        });
     }
 }
